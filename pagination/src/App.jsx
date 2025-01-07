15960f5d7a95c0a70e4da409de3df1a81d61e298
Shashi5657 <sangepushashidhar@gmail.com>
2025-01-07 20:35:04 +0530

    finished

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -17,7 +17,15 @@ function App() {
     fetchProducts();
   }, []);
 
-  const selectPageHandler = (selectedPage) => [setPage(selectedPage)];
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 &&
+      selectedPage !== page
+    ) {
+      setPage(selectedPage);
+    }
+  };
 
   return (
     <div>
@@ -35,15 +43,33 @@ function App() {
       )}
       {products.length > 0 && (
         <div className="pagination">
-          <span>◀️</span>
+          <span
+            className={page > 1 ? "" : "pagination__disable"}
+            onClick={() => {
+              selectPageHandler(page - 1);
+            }}
+          >
+            ◀️
+          </span>
           {[...Array(products.length / 10)].map((_, i) => {
             return (
-              <span onClick={() => selectPageHandler(i + 1)} key={i}>
+              <span
+                className={page === i + 1 ? "pagination__selected" : ""}
+                onClick={() => selectPageHandler(i + 1)}
+                key={i}
+              >
                 {i + 1}
               </span>
             );
           })}
-          <span>▶️</span>
+          <span
+            onClick={() => {
+              selectPageHandler(page + 1);
+            }}
+            className={page < products.length / 10 ? "" : "pagination__disable"}
+          >
+            ▶️
+          </span>
         </div>
       )}
     </div>
