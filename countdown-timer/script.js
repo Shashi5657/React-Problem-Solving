dda0530d79db226f595e03f3172379ebd08cc8fc
Shashi5657 <sangepushashidhar@gmail.com>
2025-01-15 10:46:54 +0530

    js countdown timer

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -12,11 +12,28 @@
   startBtn.addEventListener("click", function () {
     if (hour.value == 0 && min.value == 0 && sec.value == 0) return;
     console.log("clicked");
-    function setsssInterval() {
+    function startInterval() {
       startBtn.style.display = "none";
       stopBtn.style.display = "initial";
+
+      countdownTimer = setInterval(() => {
+        timer();
+      }, 1000);
     }
 
-    setsssInterval();
+    startInterval();
   });
+
+  function stopInterval() {}
+
+  function timer() {
+    if (hour.value == 0 && min.value == 0 && sec.value == 0) {
+      hour.value = "";
+      min.value = "";
+      sec.value = "";
+      stopInterval();
+    } else if (sec != 0) {
+      sec.value = `${sec.value <= 10 ? "0" : ""}${sec.value - 1}`;
+    }
+  }
 })();
