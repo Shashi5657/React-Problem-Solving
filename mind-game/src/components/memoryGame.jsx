3d9ef79ec61dc4261b503bbefe2223aeb48891ce
shashi5657 <sangepushashidhar@gmail.com>
2025-01-23 22:49:50 +0530

    mind game

--- mind-game/src/components/memoryGame.jsx
+++ mind-game/src/components/memoryGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const MemoryGame = () => {
   const [gridSize, setGridSize] = useState(4);
@@ -8,7 +8,27 @@ const MemoryGame = () => {
   const [won, setWon] = useState(false);
   const [disabled, setDisabled] = useState(false);
 
-  const handleGridChange = (e) => {};
+  const handleGridSizeChange = (e) => {
+    const size = parseInt(e.target.value);
+    if (size >= 2 && size <= 10) setGridSize(size);
+  };
+
+  const initializeGame = () => {
+    const totalCards = gridSize * gridSize;
+    const pairCount = Math.floor(totalCards / 2);
+    const numbers = [...Array(pairCount).keys()].map((n) => n + 1);
+    const shuffledCards = [...numbers, ...numbers]
+      .sort(() => Math.random() - 0.5)
+      .slice(0, totalCards)
+      .map((number, index) => {
+        id: index, number;
+      });
+    console.log(shuffledCards, "num");
+  };
+  useEffect(() => {
+    initializeGame();
+  }, [gridSize]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <h1 className="text-3xl font-bold mb-6">Memory Game</h1>
@@ -23,7 +43,7 @@ const MemoryGame = () => {
           min="2"
           max="10"
           value={gridSize}
-          onChange={() => handleGridChange(e)}
+          onChange={handleGridSizeChange}
           className="border-2 border-gray-300 rounded px-2 py-1"
         />
       </div>
