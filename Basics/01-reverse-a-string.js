529a00614b3ba42e55b6c632c74ab995eae61057
shashi5657 <sangepushashidhar@gmail.com>
2025-02-11 10:58:06 +0530

    selection sort

--- Basics/01-reverse-a-string.js
+++ Basics/01-reverse-a-string.js
@@ -156,4 +156,26 @@ function bubbleSort(arr) {
   return arr
 }
 
-console.log(bubbleSort([21, 8, 16, 5]));
+
+
+function selectionSort(arr){
+  let n = arr.length
+
+  for(i = 0; i<n-1; i++){
+    let minIndex = i
+
+    for(j = i+1; j< n; j++){
+      if(arr[j] < arr[minIndex]){
+        minIndex = j
+      }
+    }
+
+    if(minIndex !== i){
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+  }
+
+  return arr
+}
+
+console.log(selectionSort([21, 8, 16, 5]));
