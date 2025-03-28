0fa556d6b1f711c52a67422e25856f8a7bde2f94
shashi5657 <sangepushashidhar@gmail.com>
2025-03-28 15:41:55 +0530

    call,apply,bind

---
+++ call-apply-bind/call-apply-bind.js
@@ -0,0 +1,29 @@
+//Before diving into call, apply, and bind, it's important to understand how this works.
+//this refers to the object that is executing the current function.
+
+const person1 = { name: "Shashi" };
+
+const person2 = { name: "Vinay" };
+
+function intro(age) {
+  console.log(`Hello, My name is ${this.name} and I am ${age} years`);
+}
+
+intro.call(person1, 24);
+
+intro.apply(person2, [25]);
+
+const introduction = intro.bind(person1, 30);
+
+introduction();
+
+// async makes a function return a promise automatically.
+// await pauses execution until the promise resolves.
+
+async function getData() {
+  console.log("fetching data...");
+  const data = await fetchData();
+  console.log(data);
+}
+
+getData();
