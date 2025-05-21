503b2d56d0292ecc655e93019c6f21d493774622
shashi5657 <sangepushashidhar@gmail.com>
2025-05-21 17:18:57 +0530

    array methods

--- scenerio-based-questions/src/components/ArrayMethods.jsx
+++ scenerio-based-questions/src/components/ArrayMethods.jsx
@@ -165,10 +165,22 @@ const ArrayMethods = () => {
   console.log(array1); //[65, 44, 33, 5, 2]
 
   //slice
-  console.log(array1.slice(1, 2)); //[44]
+  console.log(array1.slice(1, 3)); //[44,33]
+
   //some
+  const array = [1, 2, 3, 4, 5];
+  // Checks whether an element is even
+  const even = (element) => element % 2 === 0;
+  console.log(array.some(even));
+  // Expected output: true
+
   //sort
+  console.log(array1.sort((a, b) => b - a)); //[65, 44, 33, 5, 2]
+
   //splice
+
+  array1.splice(5, 0, 1);
+  console.log(array1);
   //toReversed
   //toSorted
 
